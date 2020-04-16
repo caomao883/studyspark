f852d7e3d713f35b38646830c262c6b1f185e8f8
ouhuihao <80213526@qq.com>
2020-04-16 21:15:25 +0800

    reduceByKey
    Signed-off-by: ouhuihao <80213526@qq.com>

--- src/main/scala/StudySpark.scala
+++ src/main/scala/StudySpark.scala
@@ -2,7 +2,8 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object StudySpark {
   def main(args: Array[String]): Unit = {
-    groupByKey
+    //groupByKey
+    reduceByKey
   }
   def groupByKey(): Unit = {
     val conf = new SparkConf().setMaster("local").setAppName("groupByKey")
@@ -13,5 +14,14 @@ object StudySpark {
       x._2.foreach(value=>println(value))
       println("======================================")
     }
+  }
+  def reduceByKey(): Unit = {
+    val conf = new SparkConf().setMaster("local").setAppName("reduceByKey")
+    val sc = new SparkContext(conf)
+    sc.parallelize(Array(Tuple2("class1",33),Tuple2("class2",22),Tuple2("class1",44),Tuple2("class3",55)))
+        //.reduceByKey(_+_).foreach(x=>println(x._1,x._2))
+      .reduceByKey{
+      (x,y)=>x+y
+    }.foreach(x=>println(x))
   }
 }
